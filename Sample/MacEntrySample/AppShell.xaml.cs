848cfedcb7a6407f3727cd879f5263172a473a08
Anandhan <anandhan.rajagopal@syncfusion.com>
2023-12-18 16:01:42 +0530

    Added sample

---
+++ Sample/MacEntrySample/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MacEntrySample
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Sample/MacEntrySample/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace MacEntrySample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            entry.Text = "Text Updated from Code Behind but color doest not applied";
+        }
+    }
+
+}
